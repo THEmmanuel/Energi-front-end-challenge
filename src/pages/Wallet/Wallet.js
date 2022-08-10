73c244b52627990329fcb3801a506aba885f0404
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-08-10 20:43:07 +0100

    Add state

--- src/pages/Wallet/Wallet.js
+++ src/pages/Wallet/Wallet.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import style from './Wallet.module.css';
 import Metamask from '../../../assets/metamask.svg'
 
 const Wallet = () => {
+	const [hasMetamask, setHasMetamask] = useState(true);
+	const [accountAddress, setAccountAddress] = useState('')
+	const [accountBalance, setAccountBalance] = useState('')
+	const [isConnected, setIsConnected] = useState('')
+
+
+	// Destructure the Ethereum object from the window object
+	useEffect(() => {
+		const { ethereum } = window;
+		const checkMetamaskHandler = async () => {
+			!ethereum ? setHasMetamask(false) : setHasMetamask(true)
+		};
+		checkMetamaskHandler()
+	}, [])
+
+	const connectWalletHandler = async () => {
+
+	}
+
 	return (
 		<div className={style.Wallet}>
 			<div className={style.WalletWrapper}>
